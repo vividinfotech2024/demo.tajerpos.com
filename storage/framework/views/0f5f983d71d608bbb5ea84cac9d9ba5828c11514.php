53a6ea38f567fc3572ac228d00987583687532f5
paravindrajan2002 <paravindrajan2002@gmail.com>
2024-03-01 12:08:50 +0530

    Revert "delete commit"
    This reverts commit 949bc2d9d610db629c34807159c9a8e5ca50e55e.

--- storage/framework/views/0f5f983d71d608bbb5ea84cac9d9ba5828c11514.php
+++
@@ -1,5 +0,0 @@
-<footer class="main-footer">
-    <div class="row">
-        <div class="col-12 text-center"><?php echo e(trans('store-admin.copyrights', ['year' => date('Y'), 'company_name' => Auth::user()->company_name])); ?></div>
-    </div>
-</footer><?php /**PATH /var/www/html/dev.tajerpos.com/resources/views/common/cashier_admin/copyright.blade.php ENDPATH**/ ?>
--- storage/framework/views/e73a4019ca8f7a6023030f517058ab4a81b32d60.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer font-xs">
-  <div class="row pb-30 pt-15">
-    <div class="col-sm-12 text-center"><?php echo e(trans('admin.copyrights', ['year' => date('Y'), 'company_name' => 'TajerPOS'])); ?></div>
-  </div>
-</footer>       
-<?php /**PATH /var/www/html/dev.tajerpos.com/resources/views/common/admin/footer.blade.php ENDPATH**/ ?>
